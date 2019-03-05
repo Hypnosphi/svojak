18b2dd1f395091d6e11a12ac8e8efb48541a62fb
Hypnosphi <Filipp.Riabchun@jetbrains.com>
2019-03-05 02:11:57 +0100

    Fix deployment

--- server/tts.ogg.js
+++ server/tts.ogg.js
@@ -1,5 +1,5 @@
-require('dotenv').config()
 const fs = require('fs')
+const url = require('url')
 const querystring = require('querystring')
 
 const axios = require('axios')
@@ -10,20 +10,27 @@ const token =
 const folderId = process.env.FOLDER_ID
 
 module.exports = async (req, res) => {
-  const {text} = req.query
-  const {data, headers} = await axios(
-    `https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize`,
-    {
-      method: 'post',
-      data: querystring.stringify({text, voice: 'alyss', folderId}),
-      headers: {
-        Authorization: `Bearer ${await yandexAuth()}`,
+  const {search} = url.parse(req.url)
+  const {text} = querystring.parse(search.slice(1))
+  try {
+    const {data, headers} = await axios(
+      `https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize`,
+      {
+        method: 'post',
+        data: querystring.stringify({text, voice: 'alyss', folderId}),
+        headers: {
+          Authorization: `Bearer ${await yandexAuth()}`,
+        },
+        responseType: 'stream',
       },
-      responseType: 'stream',
-    },
-  )
+    )
 
-  res.setHeader('Content-Type', headers['content-type'])
+    res.setHeader('Content-Type', headers['content-type'])
 
-  data.pipe(res)
+    data.pipe(res)
+  } catch (e) {
+    console.error(e)
+    res.statusCode = 500
+    res.end()
+  }
 }
